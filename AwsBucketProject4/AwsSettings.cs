a5b7da97cac0a08f8f6543b87e1605a9d659bca7
navid khodaveisi <navidliverpool@yahoo.com>
2021-08-12 14:09:05 +0100

    commit1

---
+++ AwsBucketProject4/AwsSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AwsBucketProject4
+{
+    public class AwsSettings
+    {
+        public string AwsAccessKeyID { get; set; }
+        public string AwsSecretAccessKey { get; set; }
+        public RegionEndpoint Endpoint { get; set; } = RegionEndpoint.USEast1;
+        public string BucketName { get; set; }
+    }
+}
--- AwsBucketProject4/ProcessEmails.cs
+++ AwsBucketProject4/ProcessEmails.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Amazon.S3;
+using MimeKit;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +10,33 @@ namespace AwsBucketProject4
 {
     public class ProcessEmails
     {
-        public void EmailProcesser()
+        public void EmailProcesser(AwsSettings awsSettings)
         {
+            using (var s3Client = new AmazonS3Client(awsSettings.AwsAccessKeyID, awsSettings.AwsSecretAccessKey, awsSettings.Endpoint))
+            {
 
-        }
+                var objectList = s3Client.ListObjects(awsSettings.BucketName);
+                foreach (var s3Object in objectList.S3Objects)
+                {
+
+                    using (var retrievedObject = s3Client.GetObject(s3Object.BucketName, s3Object.Key))
+                    {
+
+                        MimeMessage message = MimeMessage.Load(retrievedObject.ResponseStream);
+
+                        EmailContent result = new EmailContent()
+                        {
+                            Email = retrievedObject.Key,
+                            Subject = message.Subject,
+                            Date = message.Date,
+                            From = message.From,
+                            To = message.To,
+                            Cc = message.Cc,
+                            Headers = message.Headers,
+                            Body = message.TextBody
+                        };
+
+                        EmailList.Add(result);
+                    }
     }
 }
