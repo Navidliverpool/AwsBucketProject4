1adfdf14aad38175edc44d18f04bc7a0ba380fe4
navid khodaveisi <navidliverpool@yahoo.com>
2021-08-12 14:27:11 +0100

    commit7. Display Dataset2 added

--- AwsBucketProject4/Program.cs
+++ AwsBucketProject4/Program.cs
@@ -33,6 +33,22 @@ namespace AwsBucketProject4
                 Console.WriteLine($"Cc:  {email.Cc}");
                 Console.WriteLine("-----------------------------------------------------------");
             }
+
+            //Display Dataset 2
+            Console.WriteLine($"Last Year: {processEmails.LastYearCount}");
+            Console.WriteLine($"This Year: {processEmails.ThisYearCount}");
+            Console.WriteLine($"This Month: {processEmails.ThisMonthCount}");
+            Console.WriteLine($"This Week: {processEmails.ThisWeekCount}");
+            Console.WriteLine($"Total Recipients: ");
+            Console.WriteLine($"To Count: {processEmails.ToCount}");
+            Console.WriteLine($"CC Count: {processEmails.CcCount}");
+            Console.WriteLine($"Subjects Contains Confusion: {processEmails.SubjectsContainsConfusionCount}");
+            Console.WriteLine($"Messages Contains Header x-gt-settings: {processEmails.MessagesContainsHeader_x_gt_settings}");
+            Console.WriteLine($"Messages Contains Three Musketeers: {processEmails.MessagesContainsThreeMusketeers}");
+            Console.WriteLine($"Sender From space.corp: ");
+            Console.WriteLine($"Errors: ");
+
+            Console.ReadLine();
         }
     }
 }
