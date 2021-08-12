ceb195ae5a699455b4e884b6ccd1928478834065
navid khodaveisi <navidliverpool@yahoo.com>
2021-08-12 14:22:50 +0100

    commit5. ProcessEmail ready to display dataset1

--- AwsBucketProject4/Program.cs
+++ AwsBucketProject4/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,28 @@ namespace AwsBucketProject4
     {
         static void Main(string[] args)
         {
+            //Process Email
+            var processEmails = new ProcessEmails();
+
+            AwsSettings awsSettings = new AwsSettings()
+            {
+                AwsAccessKeyID = ConfigurationManager.AppSettings["awsAccessKeyID"],
+                AwsSecretAccessKey = ConfigurationManager.AppSettings["awsSecretAccessKey"],
+                BucketName = ConfigurationManager.AppSettings["bucketName"]
+            };
+
+            //Display Dataset 1
+            processEmails.EmailProcesser(awsSettings);
+            foreach (var email in processEmails.EmailList)
+            {
+                Console.WriteLine($"Email:  {email.Email}");
+                Console.WriteLine($"Subject:  {email.Subject}");
+                Console.WriteLine($"Data:  {email.Date}");
+                Console.WriteLine($"From:  {email.From}");
+                Console.WriteLine($"To:  {email.To}");
+                Console.WriteLine($"Cc:  {email.Cc}");
+                Console.WriteLine("-----------------------------------------------------------");
+            }
         }
     }
 }
